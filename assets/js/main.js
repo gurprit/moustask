1af21a0b4078dc8fb1b1ff010170d166f99ec391
gurprit <gurprit.sahota@gmail.com>
2021-03-31 13:52:21 +0100

    more cleaning and refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,58 +7,58 @@ const staffListCont = document.querySelector('[el="staff-list"]');
 var isPopulated = false;
 
 viewStaffBtn.addEventListener('click', function (event) {
-  if (isPopulated == false) {
+  if (isPopulated === true) { 
+    return; 
+  }
 
-    Promise.all([staffData]).then((values) => {
-      
-      const valuesAr = values[0];
+    staffData.then(values => {
 
-      for (var i = 0, valuesArLen = valuesAr.length; i < valuesArLen; i++) {
+      for (var i = 0, valuesLen = values.length; i < valuesLen; i++) {
 
-        if (valuesAr[i] == null ) {
-          console.log("no data")
-        } else {
-          const staffName = valuesAr[i].name;
-          const staffOccupation = valuesAr[i].occupation;
-          const staffTeam = valuesAr[i].team;  
-          const staffProfile = valuesAr[i].img;      
-    
-          const name = document.createTextNode(staffName);
-          const occupation = document.createTextNode(staffOccupation);
-          const team = document.createTextNode(staffTeam);
-    
-          const nameCont = document.createElement("span");
-          const occupationCont = document.createElement("span");
-          const teamCont = document.createElement("span");     
-          const profile = document.createElement("img");            
-    
-          nameCont.appendChild(name);
-          occupationCont.appendChild(occupation);
-          teamCont.appendChild(team);
-          profile.src=staffProfile;
-    
-          const li = document.createElement("li");
-          li.classList.add("staff-member");
-    
-          const div = document.createElement("div");
-          div.classList.add("staff-member__inner");
-    
-          li.appendChild(div);
-          div.appendChild(profile);
-          div.appendChild(nameCont);
-          div.appendChild(occupationCont);
-          div.appendChild(teamCont);
-          
-          staffListCont.appendChild(li);
-
-          isPopulated = true;
+        if (values[i] == null ) {
+          console.log("no data");
+          continue; 
         }
 
+        const staffName = values[i].name;
+        const staffOccupation = values[i].occupation;
+        const staffTeam = values[i].team;  
+        const staffProfile = values[i].img;      
+  
+        const name = document.createTextNode(staffName);
+        const occupation = document.createTextNode(staffOccupation);
+        const team = document.createTextNode(staffTeam);
+  
+        const nameCont = document.createElement("span");
+        const occupationCont = document.createElement("span");
+        const teamCont = document.createElement("span");     
+        const profile = document.createElement("img");            
+  
+        nameCont.appendChild(name);
+        occupationCont.appendChild(occupation);
+        teamCont.appendChild(team);
+        profile.src=staffProfile;
+  
+        const li = document.createElement("li");
+        li.classList.add("staff-member");
+  
+        const div = document.createElement("div");
+        div.classList.add("staff-member__inner");
+  
+        li.appendChild(div);
+        div.appendChild(profile);
+        div.appendChild(nameCont);
+        div.appendChild(occupationCont);
+        div.appendChild(teamCont);
+        
+        staffListCont.appendChild(li);
+
+        isPopulated = true;
 
       }
 
     });
-}
+
 });
 
 
